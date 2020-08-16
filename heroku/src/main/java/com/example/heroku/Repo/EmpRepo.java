cdc076babcfea9ee9c2ed4784565f9711ce5e493
DKC0015 <arundinesharun7@gmail.com>
2020-08-16 16:01:01 +0530

    master

--- heroku/src/main/java/com/example/heroku/Repo/EmpRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.heroku.Repo;
-
-import com.example.heroku.Model.EmployeeModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmpRepo extends JpaRepository<EmployeeModel, Long> {
-}
